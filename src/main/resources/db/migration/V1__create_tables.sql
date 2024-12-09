d6a6d424ffca826fb7da6e89cbc92653fde45586
murilovantini <m.henrique.vantini18@gmail.com>
2024-12-09 13:17:00 -0300

    feat: criar migration de criação de tabelas

---
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -0,0 +1,67 @@
+CREATE TABLE users (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    photo VARCHAR(255),
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE league (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    user_id BIGINT NOT NULL,
+    course_id BIGINT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    CONSTRAINT fk_league_user FOREIGN KEY (user_id) REFERENCES user (id),
+    CONSTRAINT fk_league_course FOREIGN KEY (course_id) REFERENCES course (id)
+);
+
+CREATE TABLE category (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    color VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE course (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE course_category (
+    course_id BIGINT NOT NULL,
+    category_id BIGINT NOT NULL,
+    PRIMARY KEY (course_id, category_id),
+    FOREIGN KEY (course_id) REFERENCES course(id),
+    FOREIGN KEY (category_id) REFERENCES category(id)
+);
+
+CREATE TABLE game (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    category_id BIGINT NOT NULL,
+    FOREIGN KEY (category_id) REFERENCES category(id)
+);
+
+CREATE TABLE question (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    question VARCHAR(255) NOT NULL,
+    is_correct BOOLEAN NOT NULL
+);
+
+CREATE TABLE answer (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    is_correct BOOLEAN NOT NULL,
+    question_id BIGINT NOT NULL,
+    FOREIGN KEY (question_id) REFERENCES question(id)
+);
+
+CREATE TABLE game_question (
+    match_id BIGINT NOT NULL,
+    question_id BIGINT NOT NULL,
+    PRIMARY KEY (match_id, question_id),
+    FOREIGN KEY (match_id) REFERENCES match(id),
+    FOREIGN KEY (question_id) REFERENCES question(id)
+);
